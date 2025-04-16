4092732b1250e37706f52d0b17d72d6d9d3ea538
Karlos Miguel Serrano <karlos_serrano2002@yahoo.com>
2025-04-17 03:09:14 +0800

    feat: creat database user and table

--- my-app/app/(auth)/sign-up.tsx
+++ my-app/app/(auth)/sign-up.tsx
@@ -7,6 +7,7 @@ import { Link, router } from "expo-router";
 import { ReactNativeModal } from "react-native-modal";
 import OAuth from "../components/OAuth";
 import { useSignUp } from "@clerk/clerk-expo";
+import { fetchAPI } from "@/lib/fetch";
 
 const SignUp = () => {
   const { isLoaded, signUp, setActive } = useSignUp();
@@ -64,7 +65,14 @@ const SignUp = () => {
       // If verification was completed, set the session to active
       // and redirect the user
       if (signUpAttempt.status === "complete") {
-        // TODO: create a database user!
+        await fetchAPI("/(api)/user", {
+          method: "POST",
+          body: JSON.stringify({
+            name: form.name,
+            email: form.email,
+            clerkId: signUpAttempt.createdSessionId,
+          }),
+        });
 
         await setActive({ session: signUpAttempt.createdSessionId });
         setVerification({ ...verification, state: "success" });
