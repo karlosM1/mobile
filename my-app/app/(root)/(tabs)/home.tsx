bcf034853a260ed42f44bd66342b35f3844d7aec
Karlos Miguel Serrano <karlos_serrano2002@yahoo.com>
2025-04-17 02:53:33 +0800

    feat: neon serverless database and fetch function

--- my-app/app/(root)/(tabs)/home.tsx
+++ my-app/app/(root)/(tabs)/home.tsx
@@ -1,25 +1,17 @@
-import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
-import { Link } from "expo-router";
+import { SignedIn, useUser } from "@clerk/clerk-expo";
 import { Text, View } from "react-native";
 import { SignOutButton } from "@/app/components/SignOutButton";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Page() {
+export default function Home() {
   const { user } = useUser();
 
   return (
-    <View>
+    <SafeAreaView>
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
         <SignOutButton />
       </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
-    </View>
+    </SafeAreaView>
   );
 }
